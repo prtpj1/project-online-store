c68ed9d09b7806bb6d078a72b0260b3fc740c9ad
Christian Mendes Pereira <christianp3m@gmail.com>
2022-03-10 18:35:39 -0300

    Requirement 4 done

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
-import * as api from './services/api';
 import Home from './components/Home';
 import ShoppingCart from './components/ShoppingCart';
-// Alteração Paulo
 
 class App extends Component {
-  async componentDidMount() {
-    await api.getCategories();
-    await api.getProductsFromCategoryAndQuery();
-  }
-
   render() {
     return (
       <BrowserRouter>
