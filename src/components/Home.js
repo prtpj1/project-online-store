47e0924d09e1734bf14d857eea1e06fb48aaa15f
Christian Mendes Pereira <christianp3m@gmail.com>
2022-03-10 20:26:22 -0300

    Requirement 5 done

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import * as api from '../services/api';
+import Card from './Card';
 
 class Home extends Component {
   constructor(props) {
     super(props);
     this.state = {
       allCategories: [],
+      query: '',
+      products: [],
     };
   }
 
@@ -16,8 +19,24 @@ class Home extends Component {
     await api.getProductsFromCategoryAndQuery();
   }
 
+  handleState = ({ target }) => {
+    const { name, value } = target;
+    this.setState({ [name]: value });
+  }
+
+  searchProduct = async () => {
+    const { query } = this.state;
+    const products = await api.getProductsFromCategoryAndQuery(query);
+    console.log(products);
+    this.setState({ products: products.results });
+  }
+
   render() {
-    const { allCategories } = this.state;
+    const {
+      state: { allCategories, query, products },
+      handleState,
+      searchProduct,
+    } = this;
     return (
       <div>
         <p data-testid="home-initial-message">
@@ -36,6 +55,39 @@ class Home extends Component {
             }
           </ul>
         </aside>
+        <main>
+          <section>
+            <input
+              type="text"
+              data-testid="query-input"
+              name="query"
+              value={ query }
+              onChange={ handleState }
+            />
+            <button
+              type="button"
+              data-testid="query-button"
+              onClick={ searchProduct }
+            >
+              Pesquisar
+            </button>
+          </section>
+          <section>
+            {
+              products ? (
+                products.map(({ title, thumbnail, price }) => (
+                  <li key={ title }>
+                    <Card name={ title } image={ thumbnail } price={ price } />
+                  </li>
+                ))
+              ) : (
+                <div>
+                  <p>Nenhum produto foi encontrado</p>
+                </div>
+              )
+            }
+          </section>
+        </main>
       </div>
     );
   }
