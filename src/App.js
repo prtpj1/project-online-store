8e504e63fe96180231756fdf1a2dbcaedb3add2c
Christian Mendes Pereira <christianp3m@gmail.com>
2022-03-10 17:58:18 -0300

    Requirement 3 done

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
 import * as api from './services/api';
 import Home from './components/Home';
+import ShoppingCart from './components/ShoppingCart';
 // Alteração Paulo
 
 class App extends Component {
@@ -16,6 +17,7 @@ class App extends Component {
       <BrowserRouter>
         <Switch>
           <Route exact path="/" component={ Home } />
+          <Route exact path="/shopping-cart" component={ ShoppingCart } />
         </Switch>
       </BrowserRouter>
     );
