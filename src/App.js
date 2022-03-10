5ca3b00b5b1ce827b6cdc6d6632bff3d79a53321
Christian Mendes Pereira <christianp3m@gmail.com>
2022-03-10 17:18:14 -0300

    Requirement 1 done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import * as api from './services/api';
 // Alteração Paulo
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>Edit src/App.js and save to reload.</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  async componentDidMount() {
+    await api.getCategories();
+    await api.getProductsFromCategoryAndQuery();
+  }
+
+  render() {
+    return (
+      <div>
+        <p>teste</p>
+      </div>
+    );
+  }
 }
 
 export default App;
