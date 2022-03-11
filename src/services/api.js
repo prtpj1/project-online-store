339dc293c192fdaffe44fa11c0d0aefa0cf382ee
Christian Mendes Pereira <christianp3m@gmail.com>
2022-03-11 16:59:34 -0300

    Requirement 6 done

--- src/services/api.js
+++ src/services/api.js
@@ -11,3 +11,10 @@ export async function getProductsFromCategoryAndQuery(categoryId, query) {
   const items = response.json();
   return items;
 }
+
+export async function getAllProductsOfACategory(categoryId) {
+  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+  const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}`);
+  const items = response.json();
+  return items;
+}
